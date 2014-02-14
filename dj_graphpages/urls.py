9dc8ca748e89927e1b8225ad8c081932eee62fbc
Richard Bell <rbell01824@gmail.com>
2014-02-14 14:27:03 -0500

    master baby name data

--- dj_graphpages/urls.py
+++ dj_graphpages/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'dj_graphpages.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^import_baby_names/', include('baby_names.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
