113712398e91b888d6035ec8698d3e5db1cac29b
Richard Bell <rbell01824@gmail.com>
2014-02-19 10:04:33 -0500

    Proper admin for CIA

--- test_data/admin.py
+++ test_data/admin.py
@@ -26,6 +26,22 @@ from .models import CIA, Countries
 
 
 class CIAAdmin(admin.ModelAdmin):
+    model = CIA
+    search_fields = ('name', 'country_code')
+    # list_filter = ('is_active', 'users', 'created_by', 'created_on', 'status')
+    # ordering = ('name')
+    list_display = ('name', 'country_code', 'total_area', 'coastline',
+                    'population', 'birth_rate', 'p_growth',
+                    'phone_mobiles', 'internet_users')
+    fields = (('name', 'country_code'),
+              ('total_area', 'land_area', 'water_area'),
+              ('coastline', 'total_border'),
+              ('labor_force', 'p_young', 'p_adult', 'p_old', 'p_growth'),
+              ('phone_mobiles', 'phone_mainlines'),
+              ('internet_users', 'isps'),
+              ('birth_rate', 'death_rate'))
+    # filter_horizontal = ('users',)
+    save_on_top = True
     pass
 admin.site.register(CIA, CIAAdmin)
 
